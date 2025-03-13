8f4d35530f6eb6667edbc578365a4d85459821d4
Srijani-Edara <srijaniedara888@gmail.com>
2025-03-12 17:09:28 -0700

    Project Skeleton

---
+++ gaming-with-spring/src/main/java/com/learning/gaming_with_spring/GamingWithSpringApplication.java
@@ -0,0 +1,13 @@
+package com.learning.gaming_with_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamingWithSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamingWithSpringApplication.class, args);
+	}
+
+}
